4f2630867a741b36884987af30d77a19d296a0a7
Sébastien Carret <sebastien.carret78@gmail.com>
2021-10-15 09:49:37 +0200

    ajout p OK

--- script.js
+++ script.js
@@ -5,10 +5,21 @@ document.getElementById("title").textContent = "Hello World";
 var p = document.getElementsByClassName('text');
 var container = document.getElementById("text-container").childNodes;
 
-console.log(container);
+// console.log(container);
 
 for (let i = 0; i < p.length; i++){
     p[i].addEventListener('click', function(){
         this.remove()
     })
-}
+};
+
+var button = document.getElementById('btn');
+button.addEventListener('click', function(){
+    var p = document.createElement('p');
+    document.getElementById("text-container").appendChild(p);
+    p.className = "text";
+    p.textContent = document.getElementById('input').value;
+    p.addEventListener('click', function(){
+        this.remove()
+    })
+})
